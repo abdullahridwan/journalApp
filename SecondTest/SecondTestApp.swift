8b24fd911cdb1d94c031fa2504881f8a5a0b2c16
Abdullah Ridwan <abdullahridwan73@icloud.com>
2022-01-16 20:53:35 -0500

    Initial Commit

---
+++ SecondTest/SecondTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondTestApp.swift
+//  SecondTest
+//
+//  Created by Abdullah Ridwan on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SecondTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
