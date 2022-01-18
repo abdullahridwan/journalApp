9ce1508e2953ee9d582334fdd5cdddd5c0e1999c
Abdullah Ridwan <abdullahridwan73@icloud.com>
2022-01-17 21:05:29 -0500

    Finished basics

---
+++ SecondTest/DebounceTest.swift
@@ -0,0 +1,8 @@
+//
+//  DebounceTest.swift
+//  SecondTest
+//
+//  Created by Abdullah Ridwan on 1/17/22.
+//
+
+import Foundation
--- SecondTest/Views/SeeNote.swift
+++ SecondTest/Views/SeeNote.swift
@@ -11,7 +11,20 @@ struct SeeNote: View {
     @Binding var noteViewModel: NoteViewModel
     @EnvironmentObject var journalViewModel: JournalViewModel
     var body: some View {
-        TextField("\(noteViewModel.title)", text: $noteViewModel.title)
+        VStack{
+            TextField("\(noteViewModel.title)", text: Binding(
+                get: { noteViewModel.title },
+                set: { noteViewModel.title = $0 }
+                ))
+                .padding()
+                .textFieldStyle(PlainTextFieldStyle())
+            TextEditor(text: $noteViewModel.content)
+                .onDebouncedChange(of: $noteViewModel.content, debounceFor: 1, perform: {_ in 
+                    journalViewModel.updateTodo(tdvm: noteViewModel)
+                })
+                            .padding()
+            
+        }
     }
 }
 
@@ -21,6 +34,13 @@ struct SeeNote: View {
 //        SeeNote(noteViewModel: contextViewModel() )
 //    }
 //}
+
+extension NSTextField {
+        open override var focusRingType: NSFocusRingType {
+                get { .none }
+                set { }
+        }
+}
 
 
 
